aab690119ccfac92958eadb096ea8d891cbb61b8
Gitsvh <ashermutale82@gmail.com>
2023-03-12 15:57:28 -0700

    5-print

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-no_c.py
+
+def no_c(my_string):
+    listofchars = list(my_string)
+    for char in listofchars:
+        if char == 'c' or char == 'C':
+            listofchars.remove(char)
+     return("".join(listofchars))

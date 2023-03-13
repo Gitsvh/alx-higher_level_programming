a4674b3ecd8a444b63577ac1d3c3c173ac90f500
Gitsvh <ashermutale82@gmail.com>
2023-03-13 11:42:39 -0700

    9-print

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 9-max_integer.py
+
+def max_integer(my_list=[]):
+    if my_list:
+        imax = my_list[0]
+        for i in my_list:
+            if i > imax:
+                imax = i
+                return imax
+            return None

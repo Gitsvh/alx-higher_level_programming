83f5bb134d1db0e9f6593b3f5986b59dafe42cb8
Gitsvh <ashermutale82@gmail.com>
2023-03-16 23:33:30 -0700

    2-print

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for number in range(ord('a'), ord('z') + 1):
-    print("{}".format(chr(number)), end=" ")
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")

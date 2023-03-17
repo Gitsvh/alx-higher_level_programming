86bf4d2325ad94333840f2a7606560ef4856a6ac
Gitsvh <ashermutale82@gmail.com>
2023-03-16 23:36:49 -0700

    3-print

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for asher in range(ord('a'), ord('z') + 1):
-    if chr(asher) != 'q' and chr(asher) != 'e':
-        print(f"{}".format(chr(asher)) end=' ')
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
